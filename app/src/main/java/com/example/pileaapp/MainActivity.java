9fc4b8e3553e73e5411dcd355815d1fe52b6a7df
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-12-20 19:50:53 +0100

    Implemented basic Get Categories activity.

--- app/src/main/java/com/example/pileaapp/MainActivity.java
+++ app/src/main/java/com/example/pileaapp/MainActivity.java
@@ -4,11 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView categories;
+    private String url = "https://pilea-web-dev.azurewebsites.net/api/v1/Category";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        categories = (TextView) findViewById(R.id.categories);
     }
+
+
+    public void showCategories(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String>  data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String name = object.getString("plantCategory");
+
+                    data.add(name);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+
+
+            for (String row: data) {
+                String currentText = categories.getText().toString();
+                categories.setText(currentText + "\n\n" + row);
+            }
+        }
+    };
+
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
 }
