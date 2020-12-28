0dd9b8c16debf1f4665078ad0beb3a09f91f4e7f
Beti <betipisc@gmail.com>
2020-12-28 19:14:38 +0100

    Added register form validation.

--- app/src/main/java/com/example/pileaapp/RegisterActivity.java
+++ app/src/main/java/com/example/pileaapp/RegisterActivity.java
@@ -8,6 +8,8 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
@@ -21,9 +23,9 @@ public class RegisterActivity extends AppCompatActivity {
         if(validateInput()){
 
         }
-
+/*
         Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 
 
@@ -33,17 +35,43 @@ public class RegisterActivity extends AppCompatActivity {
         EditText password1 = (EditText) findViewById(R.id.editPassword_register);
         EditText password2 = (EditText) findViewById(R.id.editPassword2_register);
 
+        Pattern specailCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Pattern UpperCasePatten = Pattern.compile("[A-Z ]");
+        Pattern lowerCasePatten = Pattern.compile("[a-z ]");
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+
 
         if(!Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()){
             formValid = false;
             System.out.println("Email wrong");
         }
-        if(password1.getText().equals(password2.getText())){
+        if(!specailCharPatten.matcher(password1.getText()).find()){
             formValid = false;
-            System.out.println("password wrong");
+            System.out.println("No special characters");
         }
-        //Add a more strict validation
-        System.out.println(formValid);
+        if(!UpperCasePatten.matcher(password1.getText()).find()){
+            formValid = false;
+            System.out.println("No uppsercase characters");
+        }
+        if(!lowerCasePatten.matcher(password1.getText()).find()){
+            formValid = false;
+            System.out.println("No lowercase characters");
+        }
+        if(!digitCasePatten.matcher(password1.getText()).find()){
+            formValid = false;
+            System.out.println("No digital characters");
+        }
+        //For some reason this doesnt work
+        /*
+        if(!((password1.getText()).equals(password2.getText()))){
+            formValid = false;
+            System.out.println(password1.getText()+" password dont match "+password2.getText());
+        }*/
+
+        //Fix the password equals thing
+
+
+
         return formValid;
     }
 
