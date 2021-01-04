46d98f73060609448c36d6b4adf4c0b1d25dc12a
Beti <betipisc@gmail.com>
2021-01-04 21:43:23 +0100

    Added view plants and delete plant.

--- app/src/main/java/com/example/pileaapp/activity/PlantsActivity.java
+++ app/src/main/java/com/example/pileaapp/activity/PlantsActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pileaapp.R;
 import com.example.pileaapp.addPlantActivity;
-import com.example.pileaapp.api.models.Category;
+
 import com.example.pileaapp.api.models.Plant;
 import com.example.pileaapp.helpers.RecycleViewAdapterPlants;
 
@@ -35,10 +39,18 @@ public class PlantsActivity extends AppCompatActivity {
     CompositeDisposable compositeDisposable;
     private static final String TAG = PlantsActivity.class.getSimpleName();
 
+    static Dialog deleteDialog;
+
+    public static PlantsActivity instance;    // Some objects require this.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
+
+
         setContentView(R.layout.activity_plants);
+
 
         s1 = new ArrayList();
         recyclerView = findViewById(R.id.plantsRecycleView);
@@ -47,6 +59,14 @@ public class PlantsActivity extends AppCompatActivity {
         myRecycleViewAdapter = new RecycleViewAdapterPlants(this, s1);
         recyclerView.setAdapter(myRecycleViewAdapter);
 
+
+        deleteDialog = new Dialog(this);
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
         showPlants();
     }
 
@@ -69,19 +89,12 @@ public class PlantsActivity extends AppCompatActivity {
                         Log.d(TAG, "SUCCESS");
                         List<Plant> plants = list;
                         Log.d(TAG, "Number of plants received: " + plants.size());
-//                        for (Category category: categories) {
-//                            System.out.println(category.getPlantCategory());
-//                        }
+
 
 
                         myRecycleViewAdapter = new RecycleViewAdapterPlants(context, list);
                         recyclerView.setAdapter(myRecycleViewAdapter);
 
-                        /*
-                        for (String row: data) {
-                            String currentText = categories.getText().toString();
-                            categories.setText(currentText + "\n\n" + row);
-                        }*/
 
                     }
 
@@ -99,6 +112,86 @@ public class PlantsActivity extends AppCompatActivity {
     public void AddPlantAcitivty (View view) {
         Intent intent = new Intent(this, addPlantActivity.class);
         startActivity(intent);
+
+    }
+
+    //Delete plant
+    public void ShowDeletePopup(View v, Plant plant) {
+        Button btnClose;
+        Button btnDelete;
+        TextView text;
+        deleteDialog.setContentView(R.layout.delete_popup);
+
+        btnClose = (Button) deleteDialog.findViewById(R.id.deletePopUpBNo);
+        btnDelete = (Button) deleteDialog.findViewById(R.id.deletePopUpBYes);
+        text = (TextView) deleteDialog.findViewById(R.id.deletePopUpTVText);
+
+        text.setText("Do you really want to delete this plant?");
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                deletePlant(plant);
+                deleteDialog.dismiss();
+
+            }
+        });
+
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDialog.dismiss();
+            }
+        });
+
+        deleteDialog.show();
+    }
+
+
+    public void deletePlant(Plant selectedPlant) {
+        compositeDisposable = new CompositeDisposable();
+
+
+
+        Single<Plant> plantSingle = MainActivity.apiService.deletePlant(selectedPlant.getPlantID(), MainActivity.userLogin.getToken(), MainActivity.API_KEY);
+        plantSingle.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<Plant>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+//                        first we create a CompositeDisposable object which acts as a container for disposables
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onSuccess(Plant plant) {
+                        // data is ready and we can update the UI
+                        Log.d(TAG, "SUCCESS");
+                        Log.d(TAG, "Plant deleted: " + plant.getPlantID());
+
+                        //Toast
+                        Context context = getApplicationContext();
+                        CharSequence text = "Plant deleted.";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        showPlants();
+
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        // oops, we best show some error message
+                        Log.d(TAG, "ERROR: " + e.getMessage());
+                    }
+
+
+                });
+
 
     }
 }
