af0defcb44994aa690542a6a8374de85e9ac17a2
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2021-01-10 10:15:25 +0100

    Revert "Feature/plants functionality (#15)"
    This reverts commit 0e65bc9b29bae99767e1d99824ac39b5089c22de.

--- app/src/main/java/com/example/pileaapp/helpers/RecycleViewAdapterPlants.java
+++ app/src/main/java/com/example/pileaapp/helpers/RecycleViewAdapterPlants.java
@@ -1,7 +1,6 @@
 package com.example.pileaapp.helpers;
 
 import android.content.Context;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pileaapp.R;
@@ -50,16 +48,6 @@ public class RecycleViewAdapterPlants extends RecyclerView.Adapter<RecycleViewAd
 
         holder.myText1.setText(((Plant) data.get(position)).getName());
 
-
-        holder.plantImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("halloooo");
-                instance.DetailPlantAcitivty(v, data.get(position));
-            }
-        });
-
-
         holder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick( View view) {
@@ -69,10 +57,9 @@ public class RecycleViewAdapterPlants extends RecyclerView.Adapter<RecycleViewAd
         });
 
         holder.editButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                instance.ShowEditPopup(view, data.get(position));
+                PlantsActivity.instance.ShowEditPopup(view, data.get(position));
             }
         });
 
@@ -90,14 +77,12 @@ public class RecycleViewAdapterPlants extends RecyclerView.Adapter<RecycleViewAd
         TextView myText1;
         ImageButton deleteButton;
         ImageButton editButton;
-        ImageView plantImage;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             myText1 = itemView.findViewById(R.id.plantsTVName);
             editButton = (ImageButton) itemView.findViewById(R.id.plantsIBWater);
             deleteButton = (ImageButton) itemView.findViewById(R.id.plantsIBClose);
-            plantImage = (ImageView) itemView.findViewById(R.id.plantsIVImage);
         }
     }
 }
