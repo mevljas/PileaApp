eac8772217626c64c2a95089b07a9796bb611f50
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2021-01-10 14:20:51 +0100

    Implemented selecting images.

--- app/src/main/java/com/example/pileaapp/activity/AddPlantActivity.java
+++ app/src/main/java/com/example/pileaapp/activity/AddPlantActivity.java
@@ -1,16 +1,25 @@
 package com.example.pileaapp.activity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.util.GregorianCalendar;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -55,6 +64,7 @@ public class AddPlantActivity extends AppCompatActivity {
     private int mYear, mMonth, mDay;
     private Date selectedDate;
     Calendar c;
+    private ImageView plantImage;
 
 
 
@@ -75,6 +85,7 @@ public class AddPlantActivity extends AppCompatActivity {
         descriptionInput = (EditText) findViewById(R.id.addPlantETDescription);
         categoryInput = (Spinner) findViewById(R.id.addPlantSCategory);
         locationInput = (Spinner) findViewById(R.id.addPlantSLocation);
+        plantImage = (ImageView) findViewById(R.id.addPlantImage);
 
         instance = this;
         List<Integer> numbers = Stream.iterate(1, n -> n + 1)
@@ -291,6 +302,74 @@ public class AddPlantActivity extends AppCompatActivity {
 
 
     }
+    public void addPlantOnImageClick(View view) {
+        selectImage(instance);
+    }
+
+
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose plant image");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        plantImage.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                plantImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+            }
+        }
+    }
+
 
 
 
